7795b71fd0a0a8e20a19944ca7fbd99cadf53f52
Iryna Sinchuk <irinashabalina100393@gmail.com>
2024-01-31 08:16:18 +0200

    added

--- HW_03_01.py
+++ HW_03_01.py
@@ -5,11 +5,11 @@ def get_days_from_today(date):
     try:
         convert_date=datetime.strptime(date, "%Y-%m-%d") # input date by user
     except ValueError:
-        print("Input date has inccorect format") # error message
+        return ("Input date has inccorect format") # error message
     difference=current_date-convert_date  #difference of two dates
     return (difference.days) #output of difference
 
-print(get_days_from_today("2024-01-20"))
+print(get_days_from_today("2024.01-20"))
 
 
 
